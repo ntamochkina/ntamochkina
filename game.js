fc8ec8313e234d7fe45f350260e3d13f96bbc1ff
ntamochkina <60872844+ntamochkina@users.noreply.github.com>
2020-02-19 13:49:37 +0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
+const nameField = document.querySelector('.game-field');
+const xCount = 50;
+const yCount = 50;
+const cellWidth = 10;
+const cellHeight = 10;
+
+nameField.style.width = `${xCount * cellWidth}px`;
+nameField.style.height = `${yCount * cellHeight}px`;
 
 class Field
 {
 
     cells = [];
+    barriers = [];
 
     constructor(nameField, xCount, yCount) {
         this.nameField = nameField;
@@ -18,6 +27,8 @@ class Field
             {
                 const cell = document.createElement('div');
                 cell.classList.add('cell');
+                cell.style.width = `${cellWidth}px`;
+                cell.style.height = `${cellHeight}px`;
                 this.cells.push(cell);
                 this.nameField.appendChild(cell);
             }
@@ -36,147 +47,33 @@ class Field
         return this.yCount;
     }
 
-    //управление движением барьера 
+    // Управление движением барьера 
     manageBarriers() {
-        if (barriers.length === 0) { 
-
-            barriers.push(createBarrier('first'));
-            barriers.push(createBarrier('second'));
-            barriers = barriers.filter((barrier) => !barrier.isFullOutside(field));
-           
+        
+        // Cравнить длину с каким то числом 
+            let a = 5 - this.barriers.length;
+            let b = getRandomInRange(0, a);
+            for (let i = 0; i < a; i++) {
+                this.barriers.push(createBarrier(getRandomInRange(1,5)));
+            }
+        this.barriers = this.barriers.filter((barrier) => !barrier.isFullOutside(this));
     }
 
-    }
+    // Появление барьера 
+    emergingBarrier() {
+        if (this.barriers.length === 0) return;
 
-    //появление барьера 
-    emergingBarrier(){
-        if (barriers.length === 0) return;
-
-        for (let i = 0; i < barriers.length; i++) {
-            barriers[i].barrierMovement(field);
-            barriers[i].painting(field);
+        for (let i = 0; i < this.barriers.length; i++) {
+            this.barriers[i].barrierMovement(this);
+            this.barriers[i].painting(this);
         }
     }
 }
 
-class Car 
-{
-    
-    constructor (points)
-    {
-        this.points = points;
-        this.startPoints = JSON.parse(JSON.stringify(points));
-    }    
-
-    // Функция отрисовки машины
-    painting(field)
-    {
-        // Количество строк
-        let xCount = field.getXCount();  
-        // Отрисовка машины по координатам
-        let cells = field.getCells();
-        for (let i = 0; i < this.points.length; i++) {
-        const index = xCount * this.points[i][1] + this.points[i][0];
-        cells[index].classList.add('car');
-        }
-    }
-
-    // Функция удаления машины
-    remove(field)
-    {
-        // Количество строк
-        let xCount = field.getXCount(); 
-        // Удаление машины по координатам
-        let cells = field.getCells();
-        for (let i = 0; i < this.points.length; i++) {
-        const index = xCount * this.points[i][1] + this.points[i][0];
-        cells[index].classList.remove('car');
-        }
-    }
-
-    borderCheck(field)
-    {
-        const xCount = field.getXCount();
-        const yCount = field.getYCount();
-        let carLenght = Math.sqrt(this.points.length);
-        for (let i = 0; i < this.points.length; i++)
-        {
-            if (this.points[i][0] < 0)
-            {
-                this.points[i][0] = this.points[i][0] + carLenght;
-            }
-            else if (this.points[i][0] >= xCount) 
-            {
-                this.points[i][0] = this.points[i][0] - carLenght;
-            }
-            else if ((this.points[i][1] < 0))
-            {
-                this.points[i][1] = this.points[i][1] + carLenght;
-            }
-            else if (this.points[i][1] >= yCount) 
-            {
-                this.points[i][1] = this.points[i][1] - carLenght;
-            }
-        }
-    }
-
-    barrierCheck(field)
-    {
-        let cells = field.getCells();
-        for (let i=0; i < this.points.length; i++)
-        {
-            let point = field.getXCount() * this.points[i][1] + this.points[i][0];
-            let isBarrier = cells[point].classList.contains('barrier');
-            if (isBarrier) {
-                this.points = JSON.parse(JSON.stringify(this.startPoints));
-                return;
-            }
-        }
-    }
-    
-    traffic(keyDown, field) 
-    {
-        // Удалить машину
-        this.remove(field);
-        // Вычислить новые координаты, в зависимости от клавиши
-        switch(keyDown)
-        {
-        // Право
-            case 39: {
-                for (let i = 0; i < this.points.length; i++) {
-                    this.points[i][0] = this.points[i][0] + 1;
-                }
-                break; 
-            }
-        // Лево
-            case 37: {
-                for (let i = 0; i < this.points.length; i++) {
-                    this.points[i][0] = this.points[i][0] - 1;
-                }
-                break; 
-            }
-        // Вверх
-            case 38: {
-                for (let i = 0; i < this.points.length; i++) {
-                    this.points[i][1] = this.points[i][1] - 1;
-                }
-                break; 
-            }
-        // Вниз
-            case 40: {
-                for (let i = 0; i < this.points.length; i++) {
-                    this.points[i][1] = this.points[i][1] + 1;
-                }
-                break; 
-            }
-        }
-        this.borderCheck(field);
-        this.barrierCheck(field);
-        // Нарисовать машину
-        this.painting(field);
-   }
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let barriers = [];
+  
 
 class Barrier 
 {
@@ -243,64 +140,53 @@ class Barrier
     }
 
 
-    // проверка на достижение стены
+    // Проверка на достижение стены
     reachingTheWall(field){
         for (let i = 0; i < this.points.length; i++)
         if (this.points[i][1] >= yCount) 
             {
                 this.points[i][1] = this.points[i][1] - Math.sqrt(this.points.length);
-                //this.removeLineByLine(field);
+              
                 this.remove(field);
             }
     }
 
-    //движение барьера 
+    // Движение барьера 
     barrierMovement (field) {
 
         this.remove(field);
         for (let i = 0; i < this.points.length; i++) {
             this.points[i][1] = this.points[i][1] + 1;
         }
-        // this.reachingTheWall(field);
         this.painting(field);
         
     }
 
   
 
-//проверка выхода за пределы поля
-   isFullOutside(field) {
+    // Проверка выхода за пределы поля
+    isFullOutside(field) {
         return this.points.every((point) => point[1] > yCount)
-   }
+    }
 
 }
-
-
-
-let pointsCar = [[10, 1], [9, 2],[10, 2], [10, 3], 
-                 [10, 4], [10, 5], [10, 6], 
-                 [11, 0], [11, 1], [11, 2], [11, 3], 
-                 [11, 4], [11, 5], [11, 6], [11, 7], 
-                 [12, 0], [12, 1], [12, 2], [12, 3], 
-                 [12, 4], [12, 5], [12, 6], [12, 7], 
-                 [13, 0], [13, 1], [13, 2], [13, 3],
-                 [13, 4], [13, 5], [13, 6], [13, 7], 
-                 [14, 0], [14, 1], [14, 2], [14, 3], 
-                 [14, 4], [14, 5], [14, 6], [14, 7], 
-                 [15, 0], [15, 1], [15, 2], [15, 3], 
-                 [15, 4], [15, 5], [15, 6], [15, 7], 
-                 [16, 0], [16, 1], [16, 2], [16, 3], 
-                 [16, 4], [16, 5], [16, 6], [16, 7], 
-                 [17, 1], [17, 2], [17, 3], 
-                 [17, 4], [17, 5], [17, 6], [18,2]];
-let car = new Car(pointsCar);
-
-const nameField = document.querySelector('.game-field');
-const xCount = 100;
-const yCount = 100;
-let field = new Field(nameField, xCount, yCount);
-
-
+let pointsCar = [
+    [1, 1], [0, 2], [1, 2], [1, 3],
+    [2, 0], [2, 1], [2, 2], [2, 3],
+    [2, 4], [2, 5], [2, 6], 
+    [3, 0], [3, 1], [3, 2], [3, 3],
+    [3, 4], [3, 5], [3, 6], [3, 7],
+    [4, 0], [4, 1], [4, 2], [4, 3],
+    [4, 4], [4, 5], [4, 6], [4, 7],
+    [5, 0], [5, 1], [5, 2], [5, 3],
+    [5, 4], [5, 5], [5, 6], [5, 7],
+    [6, 0], [6, 1], [6, 2], [6, 3],
+    [6, 4], [6, 5], [6, 6], [6, 7],
+    [7, 0], [7, 1], [7, 2], [7, 3],
+    [7, 4], [7, 5], [7, 6], 
+    [8, 1], [8, 2], [9, 2], [8, 3],
+    [8, 4], [8, 5], 
+];
 
 
 const barrierShapes = {
@@ -336,68 +222,70 @@ const barrierShapes = {
     },
     second: () => {
         return [
-            [40,0], [40, -1], [40, -2], [40, -3], [40, -4],[40, -5],[40, -6],[40, -7],
-                     [41,0], [41, -1], [41, -2], [41, -3], [41, -4],[41, -5],[41, -6],[41, -7],
-                     [42,0], [42, -1], [42, -2], [42, -3], [42, -4],[42, -5],[42, -6],[42, -7],
-                     [43,0], [43, -1], [43, -2], [43, -3], [43, -4],[43, -5],[43, -6],[43, -7],
-                     [44,0], [44, -1], [44, -2], [44, -3], [44, -4],[44, -5],[44, -6],[44, -7],
-                                                                [45, -5],[45, -6],[45, -7],
-                                                                [46, -5],[46, -6],[46, -7],
-                                                                [47, -5],[47, -6],[47, -7],
-                                                                [48, -5],[48, -6],[48, -7],
-                                                                [49, -5],[49, -6],[49, -7]
-         ];
+            [24, -8], [25, -8], [26, -8], [27, -8],
+            [20, -8], [21, -8], [22, -8], [23, -8],
+
+            [24, -7], [25, -7], [26, -7], [27, -7], 
+            [20, -7], [21, -7], [22, -7], [23, -7],
+
+            [24, -6], [25, -6], [26, -6], [27, -6], 
+            [20, -6], [21, -6], [22, -6], [23, -6], 
+
+            [24, -5], [25, -5], [26, -5], [27, -5], 
+            [20, -5], [21, -5], [22, -5], [23, -5],
+
+            [24, -4], [25, -4], [26, -4], [27, -4], 
+            [20, -4], [21, -4], [22, -4], [23, -4], 
+
+            [24, -3], [25, -3], [26, -3], [27, -3], 
+            [20, -3], [21, -3], [22, -3], [23, -3], 
+
+            [24, -2], [25, -2], [26, -2], [27, -2], 
+            [20, -2], [21, -2], [22, -2], [23, -2],
+
+            [24, -1], [25, -1], [26, -1], [27, -1], 
+            [20, -1], [21, -1], [22, -1], [23, -1],
+
+            [24, 0],  [25, 0],  [26, 0],  [27, 0],
+            [20, 0],  [21, 0],  [22, 0],  [23, 0]];
+    },
+    third: () =>{
+        return [[42, -6],[43, -6] ,[44, -6] ,[45, -6],[46, -6],[47, -6],[48, -6],[49, -6],
+        [42, -5],[43, -5] ,[44, -5] ,[45, -5],[46, -5],[97, -5],[48, -5],[49, -5],
+        [48, -4],[49, -4] ,[40, -4] ,[41, -4],[42, -4],[93, -4],[44, -4],[45, -4],
+        [48, -3],[49, -3] ,[40, -3] ,[41, -3],[42, -3],[93, -3],[44, -3],[45, -3],
+        [48, -2],[49, -2] ,[40, -2] ,[41, -2],[42, -2],[93, -2],[44, -2],[45, -2],
+        [48, -1],[49, -1] ,[40, -1] ,[41, -1],[42, -1],[93, -1],[44, -1],[45, -1],
+        [48, 0],[49, 0] ,[40, 0] ,[41, 0],[92, 0],[43, 0],[44, 0],[45, 0]
+        ];
+    },
+    fourth:() =>{
+        return [
+        [9, -3], [10, -3],[11, -3],[12, -3],[13, -3],[14, -3],[15, -3],[16, -3],[17, -3],
+        [9, -2], [10, -2],[11, -2],[12, -2],[13, -2],[14, -2],[15, -2],[16, -2],[17, -2],
+        [9, -1], [10, -1],[11, -1],[12, -1],[13, -1],[14, -1],[15, -1],[16, -1],[17, -1],
+        [9, 0],  [10, 0], [ 11, 0],[12, 0], [13, 0], [14, 0], [15, 0], [16, 0], [17, 0]
+        ];
+    },
+    fifth:() =>{
+        return [
+        [10, -3],[11, -3],[12, -3],[13, -3],[14, -3],[15, -3], 
+        [10, -2],[1, -2], [12, -2],[13, -2],[14, -2],[15, -2],   
+        [10, -1],[11, -1],[12, -1],[13, -1],[14, -1],[15, -1],   
+        [10, 0], [11, 0], [12, 0], [13, 0], [14, 0],[15, 0]    
+        ];
     }
 };
 
-
-
 const createBarrier = (type) => {
-        if ('first')  {
-        return new Barrier(barrierShapes.first());
-        }
-        else 
-        if ('second') {
-        return new Barrier(barrierShapes.second());
-        }
+        if (type == 1)  return new Barrier(barrierShapes.first());
+        else if (type == 2) return new Barrier(barrierShapes.second());
+        else if (type == 3) return new Barrier(barrierShapes.third());
+        else if (type == 4) return new Barrier(barrierShapes.fourth());
+         else if (type == 5) return new Barrier(barrierShapes.fifth());
         return null;
 }
 
-let barrierPoints3 =  [ [92, 1],[93, 1] ,[94, 1] ,[95, 1],[96, 1],[97, 1],[98, 1],[99, 1],
-                        [92, 2],[93, 2] ,[94, 2] ,[95, 2],[96, 2],[97, 2],[98, 2],[99, 2],
-                        [88, 3],[89, 3] ,[90, 3] ,[91, 3],[92, 3],[93, 3],[94, 3],[95, 3],
-                        [88, 4],[89, 4] ,[90, 4] ,[91, 4],[92, 4],[93, 4],[94, 4],[95, 4],
-                        [88, 5],[89, 5] ,[90, 5] ,[91, 5],[92, 5],[93, 5],[94, 5],[95, 5],
-                        [88, 6],[89, 6] ,[90, 6] ,[91, 6],[92, 6],[93, 6],[94, 6],[95, 6],
-                        [88, 7],[89, 7] ,[90, 7] ,[91, 7],[92, 7],[93, 7],[94, 7],[95, 7]];      
-let barrier3 = new Barrier(barrierPoints3);
-
-
-
-
-let isRunning = true;
-
-let keyDown = null;
-
-
-
-function main()
-{
-    try {
-        field.painting();
-        car.painting(field);
-        barrier.painting(field);
-        barrier2.painting(field);   
-        barrier3.painting(field); 
-    } catch (e) {    
-        // Вывод ошибок в консоль           
-        console.log(e.stack);                
-        return;
-    }
-}
-
-
-main();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -406,31 +294,38 @@ function sleep(ms) {
 // Обработчик события keydown
 document.addEventListener('keydown',async function(event) {
     // Вычислить новые координаты, в зависимости от клавиши
-    keyDown = (event.keyCode);
+    let keyDown = (event.keyCode);
     // 
-    if (isRunning) 
-    {
-        await sleep(300);
-    }
-    car.traffic(keyDown, field);
+    
+   await sleep(300);
+   car.traffic(keyDown, field);
 });
-
-
 
 const countFrameSkip = 10; // 60 / countFrameSkip
 let frameSkiped = 0;
 
-function test() {
+let main = (field, car) => {
     if (++frameSkiped >= countFrameSkip) {
-       // let barriers = [];
-       field.manageBarriers();
-       field.emergingBarrier();
+        // let barriers = [];
+        field.manageBarriers();
+        field.emergingBarrier();
         car.remove(field);
         car.barrierCheck(field);
         car.painting(field);
         frameSkiped = 0;
     }
-    if (isRunning) requestAnimationFrame(test);
+    if (true) requestAnimationFrame(main.bind(null, field, car));
 }
 
-test();
+
+// альтернатива событию load
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        let car = new Car(pointsCar);
+        let field = new Field(nameField, xCount, yCount);
+
+        field.painting();
+        car.painting(field);
+        main(field, car);
+    }
+}
